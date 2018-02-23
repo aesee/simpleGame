52c8bd0557d4e06ce4cd22348730656e054c6736
Alexey Sarapulov <aesee@mail.ru>
2018-02-23 14:37:30 +0600

    Added simple animation.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ int main()
 
     sf::Sprite heroSprite;
     heroSprite.setTexture(heroTexture);
+    heroSprite.setTextureRect(sf::IntRect(0,192,96,96));
     heroSprite.setPosition(50,25);
 
     while(window.isOpen())
@@ -26,6 +27,27 @@ int main()
         {
             if(event.type == sf::Event::Closed)
                 window.close();
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            heroSprite.setTextureRect(sf::IntRect(0,96,96,96));
+            heroSprite.move(-0.1, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            heroSprite.setTextureRect(sf::IntRect(0,192,96,96));
+            heroSprite.move(0.1, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            heroSprite.setTextureRect(sf::IntRect(0,288,96,96));
+            heroSprite.move(0, -0.1);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            heroSprite.setTextureRect(sf::IntRect(0,0,96,96));
+            heroSprite.move(0, 0.1);
         }
 
         window.clear();
